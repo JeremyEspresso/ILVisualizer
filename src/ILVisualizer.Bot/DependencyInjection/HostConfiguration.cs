5236fa57e1ffd27d4d82632dc0a021f5145a3355
Jeremy <jenkenkur@gmail.com>
2021-06-09 23:18:09 +0200

    Dependencies + HostConfiguration + Startup

---
+++ src/ILVisualizer.Bot/DependencyInjection/HostConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus;
+using ILVisualizer.Application.Common;
+using ILVisualizer.Application.Common.Interfaces;
+using ILVisualizer.Application.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ILVisualizer.Bot.DependencyInjection
+{
+	public static class HostConfiguration
+	{
+		public static IHostBuilder ConfigureHost(this IHostBuilder builder)
+		{
+			IConfig applicationConfig = Config.Get();
+			
+			builder.ConfigureAppConfiguration((context, _) =>
+			{
+				context.HostingEnvironment.ApplicationName = applicationConfig.EnvironmentConfig.ApplicationName;
+				context.HostingEnvironment.EnvironmentName = applicationConfig.EnvironmentConfig.EnvironmentName;
+			});
+
+			builder.ConfigureServices((_, services) =>
+			{
+				services.AddSingleton(new DiscordClient(new DiscordConfiguration
+				{
+					Token = applicationConfig.DiscordConfig.Token,
+					TokenType = TokenType.Bot,
+					Intents = DiscordIntents.GuildMessages | DiscordIntents.Guilds,
+				}));
+
+				services.AddSingleton(applicationConfig);
+				services.AddHostedService<ILVisualizerBot>();
+				services.AddApplication();
+			});
+
+			return builder;
+		}
+	}
+}
