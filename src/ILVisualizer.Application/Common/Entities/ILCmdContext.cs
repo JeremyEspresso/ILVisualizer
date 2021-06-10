1d94982569576f408898185eb0de4fd854c20f4d
Jeremy <jenkenkur@gmail.com>
2021-06-10 23:57:01 +0200

    Our own custom command context

---
+++ src/ILVisualizer.Application/Common/Entities/ILCmdContext.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus.Entities;
+
+namespace ILVisualizer.Application.Common.Entities
+{
+	public class ILCmdContext
+	{
+		public const string Ctx = "ctx";
+
+		public ILCmdContext(DiscordMessage message)
+		{
+			Message = message;
+		}
+
+		public DiscordMessage Message { get; }
+
+		public DiscordChannel Channel => Message.Channel;
+
+		public DiscordGuild Guild => Channel.Guild;
+
+		public DiscordUser User => Message.Author;
+	}
+}
