e54d5a5a34257e34e43f53552b9414087ae1893c
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-12 10:58:04 +0100

    Fix Tests Nullable

--- tests/ILVisualizer.UnitTests/TemporaryTestClass.cs
+++ tests/ILVisualizer.UnitTests/TemporaryTestClass.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace ILVisualizer.UnitTests
 {
-	public class TemporaryTestClass
-	{
-		[Fact]
-		public void Temp()
-		{
-			Assert.True(true);
-		}
-	}
+    public class TemporaryTestClass
+    {
+        [Fact]
+        public void Temp()
+        {
+            Assert.True(true);
+        }
+    }
 }
