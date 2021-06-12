c42f5038722af16dfc851af1ef7d0c2c1922b08f
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-12 13:40:36 +0100

    Add IL instruction models

---
+++ src/ILVisualizer.Domain/Enums/IL/ILInstructionType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILVisualizer.Domain.Enums.IL
+{
+    public enum ILInstructionType
+    {
+        Ldc_I4_0,
+        Ldc_I4_1,
+        Ldc_I4_2,
+        Ldc_I4_3,
+        Ldc_I4_4,
+        Ldc_I4_5,
+        Ldc_I4_6,
+        Ldc_I4_7,
+        Ldc_I4_8,
+        Ldc_I4_M1,
+    }
+}
