7b0d8a6744e76c3fa7017d36801106aef85b1bd1
Jeremy <jenkenkur@gmail.com>
2021-06-13 14:35:11 +0200

    Some more cleanup

--- src/ILVisualizer.Application/Common/Entities/Parser/Parser.cs
+++ src/ILVisualizer.Application/Common/Entities/Parser/Parser.cs
@@ -1,10 +1,5 @@
 ﻿using ILVisualizer.Application.Common.Exceptions.Parser;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ILVisualizer.Application.Common.Entities.Parser
 {
