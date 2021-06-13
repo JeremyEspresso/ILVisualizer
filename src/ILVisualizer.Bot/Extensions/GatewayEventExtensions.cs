4a011f0f7a796e388005fd4a44fd3ce74c0148ee
Jeremy <jenkenkur@gmail.com>
2021-06-14 01:29:41 +0200

    Add GatewayEventExtensions

---
+++ src/ILVisualizer.Bot/Extensions/GatewayEventExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Remora.Discord.API.Abstractions.Gateway.Events;
+using System;
+
+namespace ILVisualizer.Bot.Extensions
+{
+	public static class GatewayEventExtensions
+	{
+		public static bool TrySlicePrefix(this IMessageCreate gatewayEvent, char prefix, out string commandContent)
+		{
+			if (gatewayEvent.Content[0] == prefix)
+			{
+				commandContent = gatewayEvent.Content[1..];
+				return true;
+			}
+
+			commandContent = null;
+			return false;
+		}		
+	}
+}
