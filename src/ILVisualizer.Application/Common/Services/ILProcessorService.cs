4108cb752ca453662da9c76b0b74e8fab893ad54
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-14 19:13:16 +0100

    Correct Mistakes

--- src/ILVisualizer.Application/Common/Services/ILProcessorService.cs
+++ src/ILVisualizer.Application/Common/Services/ILProcessorService.cs
@@ -14,8 +14,8 @@ namespace ILVisualizer.Application.Common.Services
     {
         public Step CurrentStep = new();
 
-        public ProcessorResult Result = new ProcessorResult();
-        public Stack<EvalStackItem> CurrentEvalStack = new Stack<EvalStackItem>();
+        public ProcessorResult Result = new();
+        public Stack<EvalStackItem> CurrentEvalStack = new();
 
         public ProcessorResult Process(IList<ILInstruction> instructions)
         {
@@ -26,10 +26,10 @@ namespace ILVisualizer.Application.Common.Services
                 CurrentStep = new Step();
                 ProcessInstruction(instructions[i]);
 
+                Result.Steps.Add(CurrentStep);
+
                 if (CurrentEvalStack.Count == 0)
                     InsertStatementBreak();
-
-                Result.Steps.Add(CurrentStep);
             }
 
             return Result;
@@ -45,6 +45,7 @@ namespace ILVisualizer.Application.Common.Services
         {
             switch (instruction.Type)
             {
+                case ILInstructionType.Ldc_I4_M1:
                 case ILInstructionType.Ldc_I4_0:
                 case ILInstructionType.Ldc_I4_1:
                 case ILInstructionType.Ldc_I4_2:
@@ -54,18 +55,18 @@ namespace ILVisualizer.Application.Common.Services
                 case ILInstructionType.Ldc_I4_6:
                 case ILInstructionType.Ldc_I4_7:
                 case ILInstructionType.Ldc_I4_8:
-                    CurrentStep.SinglePushed = new Int32ConstantEvalStackItem((int)instruction.Type);
+                    PushOne(new Int32ConstantEvalStackItem((int)instruction.Type));
                     break;
                 case ILInstructionType.Ldc_I4_S:
                 case ILInstructionType.Ldc_I4:
-                    CurrentStep.SinglePushed = new Int32ConstantEvalStackItem(instruction.IntArg);
+                    PushOne(new Int32ConstantEvalStackItem(instruction.IntArg));
                     break;
             }
         }
 
         public void InsertStatementBreak()
         {
-            Result.Steps.Add(new Step());
+            Result.Breaks.Add(new StatementBreak(Result.Steps.Count));
         }
 
         public EvalStackItem Pop()
